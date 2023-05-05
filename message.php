61b8cedcc4cdb9ce3453e85a6ac4111dbdcebf58
nyitrai100 <nyitrai100@gmail.com>
2023-05-05 14:35:51 +0100

    php

--- message.php
+++ message.php
@@ -7,6 +7,15 @@
 
     if(!empty($email) && !empty($message)){
         if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $receiver = "nyitrai100@gmail.com";
+            $subject = "Form: $name <$email>";
+            $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage: $message\n\nRegards, \n$name";
+            $sender = "From: $email";
+            if(mail($receiver, $subject, $body, $sender )){
+                echo "Your message has been sent!";
+            }else{
+                echo "Sorry, failed to send your message!";
+            }
 
         }else{
             echo "Enter a valid email address!";
